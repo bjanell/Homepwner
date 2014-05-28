cbe171062d0d01e0a82be334c0a67ed228e9a4b8
Ben Anello <ben@anello.us.com>
2014-05-28 17:41:34 -0600

    Add initial IOS empty application files from Xcode.

---
+++ Homepwner/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Homepwner
+//
+//  Created by Anello, Benjamin J on 5/28/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
