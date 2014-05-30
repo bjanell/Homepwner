6cf23e38982b69c4ddda956c6536c540ab50c433
Ben Anello <ben@anello.us.com>
2014-05-30 09:01:47 -0600

    Use Navigation Bar instead of Header View.

--- BNRItemsViewController.m
+++ BNRItemsViewController.m
@@ -14,7 +14,7 @@
 
 @interface BNRItemsViewController ()
 
-@property (nonatomic, strong) IBOutlet UIView *headerView;
+// @property (nonatomic, strong) IBOutlet UIView *headerView;
 
 @end
 
@@ -44,6 +44,15 @@
         // }
         UINavigationItem *navItem = self.navigationItem;
         navItem.title = @"Homepwner";
+        
+        // Create a new bar button item that will send
+        // addNewItem: to BNRItemsViewController
+        UIBarButtonItem *bbi = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addNewItem:)];
+        
+        // Set this bar button item as the right item in the navigationItem
+        navItem.rightBarButtonItem = bbi;
+        
+        navItem.leftBarButtonItem = self.editButtonItem;
     }
     return self;
 }
@@ -82,8 +91,8 @@
     [super viewDidLoad];
     
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"UITableViewCell"];
-    UIView *header = self.headerView;
-    [self.tableView setTableHeaderView:header];
+//    UIView *header = self.headerView;
+//    [self.tableView setTableHeaderView:header];
 }
 
 - (IBAction)addNewItem:(id)sender
@@ -103,33 +112,33 @@
     [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationTop];
 }
 
-- (IBAction)toggleEditingMode:(id)sender
-{
-    // If you are currently in editing mode...
-    if (self.isEditing) {
-        // Change text of button to inform user of state
-        [sender setTitle:@"Edit" forState:UIControlStateNormal];
-        
-        // Turn off editing mode
-        [self setEditing:NO animated:YES];
-    } else {
-        // Change text of button to inform user of state
-        [sender setTitle:@"Done" forState:UIControlStateNormal];
-        
-        // Enter editing mode
-        [self setEditing:YES animated:YES];
-    }
-}
+//- (UIView *)headerView
+//{
+//    // If you have not loaded the headerView yet...
+//    if (!_headerView) {
+//        // Load HeaderView.xib
+//        [[NSBundle mainBundle] loadNibNamed:@"HeaderView" owner:self options:nil];
+//    }
+//    return _headerView;
+//}
 
-- (UIView *)headerView
-{
-    // If you have not loaded the headerView yet...
-    if (!_headerView) {
-        // Load HeaderView.xib
-        [[NSBundle mainBundle] loadNibNamed:@"HeaderView" owner:self options:nil];
-    }
-    return _headerView;
-}
+//- (IBAction)toggleEditingMode:(id)sender
+//{
+//    // If you are currently in editing mode...
+//    if (self.isEditing) {
+//        // Change text of button to inform user of state
+//        [sender setTitle:@"Edit" forState:UIControlStateNormal];
+//
+//        // Turn off editing mode
+//        [self setEditing:NO animated:YES];
+//    } else {
+//        // Change text of button to inform user of state
+//        [sender setTitle:@"Done" forState:UIControlStateNormal];
+//
+//        // Enter editing mode
+//        [self setEditing:YES animated:YES];
+//    }
+//}
 
 -(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
 {
